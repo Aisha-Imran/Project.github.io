a8dac2d991b62cfe75a41d6902e373cd786804bd
Aisha-Imran <ayeshaimran054@gmail.com>
2022-08-23 04:59:35 -0700

    first commit

---
+++ app.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+
+
+
+
+
+function readMore(){
+
+    var text="Creative, elegant, practical & iconic, from the historic Speedy to the trendy Coussin, our handbags for women combine innovation with style in the tradition of the Maison’s distinctive “art of travel.” Each season brings new collections of fashionable models, expertly crafted from the House’s signature materials – Leathers and canvases: shoulder and cross-body bags, totes, clutches, evening bags, mini bags, backpacks and more."
+    var paragraph=document.getElementById('para')
+    paragraph.innerHTML=text
+}
